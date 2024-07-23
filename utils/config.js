ad44914a2c70157a63dc9d108a22bd3dcacd9505
Prashant Mishra <prashantmishra111@outlook.com>
2024-07-23 17:06:51 +0530

    base Url changed

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,5 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://bluwaterresort.in"
-    : "http://localhost:3000";
+         process.env.NODE_ENV === "production"
+           ? // ? "https://bluwaterresort.in"
+             "https://blue-water-resort.vercel.app/"
+           : "http://localhost:3000";
