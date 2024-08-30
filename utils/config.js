8a91a78684205750efe77b936d64af4d56d51d39
atharva <misalatharva407@gmail.com>
2024-08-30 14:23:16 +0530

    updated to use env var

--- utils/config.js
+++ utils/config.js
@@ -6,7 +6,11 @@
 
 
 
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_URL || "https://bluwaterresort.in"}`
-    : "http://localhost:3000";
+// export const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? `https://${process.env.VERCEL_URL || "https://bluwaterresort.in"}`
+//     : "http://localhost:3000";
+
+
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
